38681f48e7895342db8574f365042a1e270e87b6
AMIT Rawal <119728577+Amitrawal1@users.noreply.github.com>
2025-06-22 23:27:17 +0530

    new updates

---
+++ Frontend/src/landing page/Home/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css'
+export default function Hero(){
+    return(
+        <>
+            <div className="hero">
+                <div className="title">
+                    <h1>Smart Investing Starts Here</h1>
+                    <h3>BullStack: Stocks, Derivatives, Mutual Funds,</h3>
+                    <h3>ETFs, and Bonds - All in One Platform.</h3>
+                </div>
+                <div className="hero_pic"></div>
+            </div>
+        </>
+    )
+}
--- Frontend/src/landing page/Tools/Tools.jsx
+++ Frontend/src/landing page/Tools/Tools.jsx
@@ -1,20 +1,6 @@
-import './Tools.css'
-export default function Tool(){
+
+export default function Tools(){
     return(
-        <>
-        <div className="tools">
-            <div className="tools-title">
-            <h1>Calculate Brokerage & Margin Easily With These</h1>
-            </div>
-            <div className="btns">
-                <button>Brokerage Calculator</button>
-                <button>Margine Calculator</button>
-                <button>SIP Calculator</button>
-                <button>Mutual Fund Returns Calculator</button>
-                <button>SWP Calculator</button>
-            </div>
-        </div>
-        <p>Explore All Calculators →</p>
-        </>
+        <> <h2>Tools</h2></>
     )
 }
