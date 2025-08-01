95f6a795c5c361fbcb6cf80085ec3eafd1fc4022
AMIT Rawal <119728577+Amitrawal1@users.noreply.github.com>
2025-08-01 11:48:49 +0530

    connect frontend and backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,32 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser"); 
+const cors = require("cors");
 
+const {HoldingsModel} = require("./models/HoldingsModel");
+const {PositionsModel} = require("./models/PositionsModel");
 
 const PORT = process.env.PORT  || 3002;
 const url = process.env.MONGO_URL;
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/allHoldings", async(req, res)=>{
+    let allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings);
+})
+
+app.get("/allPositions", async(req,res)=>{
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+});
 
 app.listen(PORT, ()=>{
     console.log("App started");
     mongoose.connect(url);
     console.log("DB connected");
-})
+});
